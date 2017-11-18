d62e6dc34990390d2da07e17feb4ae0993471f82
redsPL <redspl@redspl.ct8.pl>
2017-11-18 19:06:36 +0100

    added support for different splashes

--- libbie.sh
+++ libbie.sh
@@ -1,74 +1,105 @@
 #!/usr/bin/env bash
 GH_REPOSITORY="https://github.com/redsPL/libbieoffice"
 if [[ $1 == "--help" || $1 == "-h" ]]; then
-	echo "libbie.sh - script for replacing LibreOffice images"
-	echo "libbie.sh needs priviliges to write to files in /usr/";echo "" # <-- too lazy to check how to do it properly
-	echo "Syntax:"
-	echo "./libbie.sh [--splash | --all] [--local]";echo ""
-	echo "Options:"
-	echo "--splash - replace ONLY splash screen"
-	echo "--all - replace icons and splash screen"
-	echo "--local - do not download images; assume files intro.png and libreoffice-[progname].png"
-	echo "          are in the working directory, and HighContrast directory with b/w icons is present." # <-- customizeable in the future?
+	printf "libbie.sh - script for replacing LibreOffice images\n"
+	printf "libbie.sh needs priviliges to write to files in /usr/\n"
+	printf "Syntax:\n"
+	printf "./libbie.sh [--splash (name) | --all] [--local]\n"
+	printf "Options:\n"
+	printf "--splash - replace ONLY splash screen\n"
+	printf "--all - replace icons and splash screen\n"
+	printf "--local - do not download images; assume files intro.png and libreoffice-[progname].png\n"
+	printf "          are in the working directory, HighContrast directory with b/w icons is present,\n"
+	printf "			and splash is in the splash directory.\n" # <-- customizeable in the future?
+	printf "Splash names:\n"
+	printf "default - the original splash with libbie instead of 5\n"
+	printf "alt - splash by KarlFish ( http://github.com/KarlFish )\n"
+	printf "all - download all splashes, install default. does nothing if executed with --local\n"
 fi
-if [[ $1 == "--splash" || $2 == "--splash" || $1 == "--all" || $2 == "--all" ]]; then
-	if [[ $1 == "--local" || $2 == "--local" ]]; then
-		cp intro.png /usr/lib/libreoffice/program/intro.png
-		cp intro.png.1 /usr/lib/libreoffice/program/intro.png # too lazy for an if
+if [[ $1 == "--splash" || $2 == "--splash" || $3 == "--splash" || $1 == "--all" || $2 == "--all" || $3 == "--all" ]]; then
+	if [[ $1 == "--splash" ]]; then
+		splash=$2;
+	elif [[ $2 == "--splash" ]]; then
+		splash=$3;
+	elif [[ $3 == "--splash" ]]; then
+		splash=$4;
 	else
-		mkdir libbie_icons
-		wget "$GH_REPOSITORY/raw/master/intro.png" -O libbie_icons/intro.png -q
-		cp libbie_icons/intro.png /usr/lib/libreoffice/program/intro.png
-		echo "Splash should now be installed!"
+		splash="default";
+	fi
+	if [[ $splash == "" ]]; then
+		splash="default";
+	fi
+	if [[ $1 == "--local" || $2 == "--local" || $3 == "--local" ]]; then
+		if [[ $splash != "all" ]]; then
+			cp "splash/$splash.png" "/usr/lib/libreoffice/program/intro.png"
+			printf "Splash should now be installed!\n"
+		else
+			printf "I've told you, i won't do a thing if executed with --local! :P\n"
+		fi
+	else
+		mkdir libbie_icons libbie_icons/splash
+		printf "Downloading splash image(s)...\n"
+		if [[ $splash == "all" ]]; then
+			wget "$GH_REPOSITORY/raw/master/splash/default.png" -O libbie_icons/splash/default.png -q
+			wget "$GH_REPOSITORY/raw/master/splash/alt.png" -O libbie_icons/splash/alt.png -q
+			cp libbie_icons/default.png /usr/lib/libreoffice/program/intro.png
+		else
+			wget "$GH_REPOSITORY/raw/master/splash/$splash.png" -O libbie_icons/splash/$splash.png -q
+			cp libbie_icons/$splash.png /usr/lib/libreoffice/program/intro.png
+		fi
+		printf "Splash should now be installed!\n"
+		
 fi
-if [[ $1 == "--all" || $2 == "--all" ]]; then
-	if [[ $1 != "--local" && $2 != "--local" ]]; then
+if [[ $1 == "--all" || $2 == "--all" || $3 == "--all" ]]; then
+	if [[ $1 != "--local" && $2 != "--local" && $3 != "--local" ]]; then
 		mkdir libbie_icons mkdir libbie_icons/HighContrast
 		cd libbie_icons
-		mv intro.png intro.png.1
-		echo "Downloading icons..."
-		wget "$GH_REPOSITORY/raw/master/libreoffice-base.png" -q
-		wget "$GH_REPOSITORY/raw/master/libreoffice-calc.png" -q
-		wget "$GH_REPOSITORY/raw/master/libreoffice-draw.png" -q
-		wget "$GH_REPOSITORY/raw/master/libreoffice-impress.png" -q
-		wget "$GH_REPOSITORY/raw/master/libreoffice-math.png" -q
-		wget "$GH_REPOSITORY/raw/master/libreoffice-writer.png" -q
-		wget "$GH_REPOSITORY/raw/master/libreoffice-misc.png" -q
-		echo "Downloading High Contrast (B/W) icons..."
+		printf "Downloading icons...\n"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-base.png" -q;printf "1/7.. \r"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-calc.png" -q;printf "2/7...\r"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-draw.png" -q;printf "3/7.  \r"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-impress.png" -q;printf "4/7.. \r"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-math.png" -q;printf "5/7...\r"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-writer.png" -q;printf "6/7.  \r"
+		wget "$GH_REPOSITORY/raw/master/libreoffice-misc.png" -q;printf "7/7 (DONE)\n"
+		printf "Downloading High Contrast (B/W) icons...\n0/7\r"
 		cd HighContrast
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-base.png" -q
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-calc.png" -q 
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-draw.png" -q
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-impress.png" -q 
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-math.png" -q
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-writer.png" -q
-		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-misc.png" -q
-		echo "OK, download done."
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-base.png" -q;printf "1/7.. \r"
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-calc.png" -q;printf "2/7...\r"
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-draw.png" -q;printf "3/7.  \r"
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-impress.png" -q;printf "4/7.. \r"
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-math.png" -q;printf "5/7...\r"
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-writer.png" -q;printf "6/7.  \r"
+		wget "$GH_REPOSITORY/raw/master/HighContrast/libreoffice-misc.png" -q;printf "7/7 (DONE)\n"
+		cd ..
+		printf "Downloading Built-in LibreOffice icons...\n0/1\r"
+		wget "$GH_REPOSITORY/raw/master/images_galaxy.zip" -q;printf "1/1 (DONE)\n"
+		printf "OK, download done.\n"
 		cd ..
 	fi
 	mkdir HighContrast 32x32 48x48 64x64 256x256 512x512 HighContrast/22x22 HighContrast/24x24 HighContrast/32x32 HighContrast/48x48 HighContrast/256x256 HighContrast/512x512
-	echo "Converting icons to..."
-	echo "32x32"
+	printf "Converting icons to...\n"
+	printf "32x32\r"
 	mogrify -resize 32x32 -path 32x32 *.png
-	echo "48x48"
+	printf "48x48\r"
 	mogrify -resize 48x48 -path 48x48/ *.png
-	echo "64x64"
+	printf "64x64\r"
 	mogrify -resize 64x64 -path 64x64/ *.png
-	echo "256x256"
+	printf "256x256\r"
 	mogrify -resize 256x256 -path 256x256/ *.png
 	cp *.png 512x512/
 	cd HighContrast
-	echo "22x22 (B/W)"
+	printf "22x22 (B/W)\r"
 	mogrify -resize 22x22 -path 22x22/ *.png
-	echo "24x24 (B/W)"
+	printf "24x24 (B/W)\r"
 	mogrify -resize 24x24 -path 24x24/ *.png
-	echo "32x32 (B/W)"
+	printf "32x32 (B/W)\r"
 	mogrify -resize 32x32 -path 32x32/ *.png
-	echo "256x256 (B/W)"
+	printf "256x256 (B/W)\n"
 	mogrify -resize 256x256 -path 256x256/ *.png
 	cp *.png 512x512/
-	echo "Conversion done."
-	echo "Copying color icons.."
+	printf "Conversion done.\n"
+	printf "Copying color icons.. "
 	cd ../32x32
 	cp -Rf *.png /usr/share/icons/hicolor/32x32/apps/
 	cp -Rf *.png /usr/share/icons/hicolor/32x32/mimetypes/
@@ -80,7 +111,8 @@ if [[ $1 == "--all" || $2 == "--all" ]]; then
 	cp -Rf *.png /usr/share/icons/hicolor/256x256/apps/
 	cd ../512x512
 	cp -Rf *.png /usr/share/icons/hicolor/512x512/apps/
-	echo "Copying B/W icons.."
+	printf "Done.\n"
+	printf "Copying B/W icons.. "
 	cd ../HighContrast/22x22
 	cp -Rf *.png /usr/share/icons/HighContrast/22x22/apps/
 	cd ../24x24
@@ -94,7 +126,8 @@ if [[ $1 == "--all" || $2 == "--all" ]]; then
 	cd ../512x512
 	cp -Rf *.png /usr/share/icons/HighContrast/512x512/apps/
 	cd ../
-	echo "Copying icons to mimetypes (this may take a while)..."
+	printf "Done.\n"
+	printf "Copying icons to mimetypes (this may take a while)...\n"
 	res="32x32"
 	type="hicolor"
 	while true; do
@@ -141,17 +174,14 @@ if [[ $1 == "--all" || $2 == "--all" ]]; then
 		fi
 	done
 	cd ..
-	echo "Copying built-in LO icons.."
-	if [[ $1 != "--local" && $2 != "--local" ]]; then
-		wget "$GH_REPOSITORY/raw/master/images_galaxy.zip"
-	fi
+	printf "Copying built-in LO icons.. "
 	cp -Rf images_galaxy.zip /usr/share/libreoffice/share/config/images_galaxy.zip
-	echo "Done!";echo""
-	echo "If everything went smoothly, you should have new icons installed."
-	echo "Enjoy! :>";echo ""
-	echo "PS: if something crashed, it doesn't mean that installation failed;"
-	echo "    Some systems don't have icon folders for some resolutions,"
-	echo "    and that's why some copy errors may occur."
-	echo "    Please also make sure that you can write to /usr/, usually sudo will do."
+	printf "Done!\n"
+	printf "If everything went smoothly, you should have new icons installed.\n"
+	printf "Enjoy! :>\n"
+	printf "PS: if something crashed, it doesn't mean that installation failed;\n"
+	printf "    Some systems don't have icon folders for some resolutions,\n"
+	printf "    and that's why some copy errors may occur.\n"
+	printf "    Please also make sure that you can write to /usr/, usually sudo will do.\n"
 	fi
 fi
